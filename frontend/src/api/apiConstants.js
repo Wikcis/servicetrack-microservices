f05b610c64d5d3cffdad70eb3acb7801c9e493c6
Wikcis <wiktor.ciszewski7734@gmail.com>
2025-06-03 20:38:03 +0200

    Add properties files to each module, refactor poms, refactor frontend ports

--- frontend/src/api/apiConstants.js
+++ frontend/src/api/apiConstants.js
@@ -1,20 +1,24 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://localhost:";
+const CLIENT_API_PORT = "8081"
+const TECHNICIAN_API_PORT = "8082"
+const SERVICEORDER_API_PORT = "8083"
+const USER_API_PORT = "8084"
 
 export const REST_API_URLS = {
     BASE_URL,
-    ONLY_LOGIN_URL: "/login",
-    ONLY_REGISTRATION_URL: "/registration",
-    ONLY_TECHNICIANS_URL: "/technicians",
-    ONLY_CLIENTS_URL: "/clients",
-    ONLY_SERVICEORDERS_URL: "/serviceorders",
-    ONLY_USER_SERVICEORDERS_URL: "/yourServiceorders",
-    ONLY_PROFILE_URL: "/profile",
-    TECHNICIANS_URL: `${BASE_URL}/technicians`,
-    CLIENTS_URL: `${BASE_URL}/clients`,
-    SERVICEORDERS_URL: `${BASE_URL}/serviceorders`,
-    LOGIN_URL: `${BASE_URL}/login`,
-    REGISTER_URL: `${BASE_URL}/registration`,
-    USERS_ME_URL: `${BASE_URL}/users/me`
+    ONLY_LOGIN_URL: `/login`,
+    ONLY_REGISTRATION_URL: `/registration`,
+    ONLY_TECHNICIANS_URL: `/technicians`,
+    ONLY_CLIENTS_URL: `/clients`,
+    ONLY_SERVICEORDERS_URL: `/serviceorders`,
+    ONLY_USER_SERVICEORDERS_URL: `/yourServiceorders`,
+    ONLY_PROFILE_URL: `/profile`,
+    TECHNICIANS_URL: `${BASE_URL}${TECHNICIAN_API_PORT}/technicians`,
+    CLIENTS_URL: `${BASE_URL}${CLIENT_API_PORT}/clients`,
+    SERVICEORDERS_URL: `${BASE_URL}${SERVICEORDER_API_PORT}/serviceorders`,
+    LOGIN_URL: `${BASE_URL}${USER_API_PORT}/login`,
+    REGISTER_URL: `${BASE_URL}${USER_API_PORT}/registration`,
+    USERS_ME_URL: `${BASE_URL}${USER_API_PORT}/users/me`
 };
 
 export const token = "token";
